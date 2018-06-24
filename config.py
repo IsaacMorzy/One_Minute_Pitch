5d1e0ab9684a79388447f35cc6c19927c53bf07d
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-06-24 10:08:15 +0300

    Add configurations of the app and db

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://isaac:morzy@localhost/pitches'
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://isaac:morzy@localhost/pitches'
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
