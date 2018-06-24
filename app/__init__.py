ea906f95353ff8660408a278f529568692cc9851
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-06-24 22:41:32 +0300

    initiliase bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-# from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -9,7 +9,7 @@ from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -28,7 +28,7 @@ def create_app(config_name):
 
 
     # Initialising flask extensions
-    # bootstrap.init_app(app)
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
 
