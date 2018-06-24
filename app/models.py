bc69ae9bc7d96d9355e981c355993979422b2cc6
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-06-24 22:39:24 +0300

    Add usermixin for login manager

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
-from .import db
+from .import db;
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User():
+
+class User(UserMixin,db.Model):
     '''
     User class to define a user in the database
     '''
@@ -8,16 +11,27 @@ class User():
     __tablename__ = 'users'
 
     #id column(primary key)
-    id=db.Column(db.integer,primary_key = True)
+    id=db.Column(db.Integer,primary_key = True)
 
     #username column for usernames
-    username = db.column(db.String(100))
-
+    name = db.Column(db.String(100))
     #email column for a user's email address
-    email = db.column(db.String(100),unique = True, index=True)
+    email = db.Column(db.String(100),unique = True, index=True)
 
-    #password_hash column for passwords
-    password_hash = db.column(db.String(100))
+    #password column for passwords
+    password = db.Column(db.String(100))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User {self.name}'
+
