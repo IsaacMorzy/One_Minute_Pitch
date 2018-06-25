4a17aa07cd61262138b672ecc8330b496da16841
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-06-25 14:35:17 +0300

    Add class PhotoProfile and Role

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@ from .import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from .import login_manager
-from sqlalchemy.sql import func 
+from datetime import datetime
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -20,7 +21,7 @@ class User(UserMixin,db.Model):
 
     #id column(primary key)
     id=db.Column(db.Integer,primary_key = True)
-
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     #username column for usernames
     name = db.Column(db.String(100))
     #email column for a user's email address
@@ -28,7 +29,9 @@ class User(UserMixin,db.Model):
 
     #password column for passwords
     password = db.Column(db.String(100))
-    profile_pic_path = db.Column(db.String())   
+    profile_pic_path = db.Column(db.String()) 
+    bio = db.Column(db.String(100))  
+    photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
 
     @property
     def password(self):
@@ -41,6 +44,27 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
 
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
     def __repr__(self):
         return f'User {self.name}'
 
+class PhotoProfile(db.Model):
+    __tablename__ = 'profile_photos'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pic_path = db.Column(db.String())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
