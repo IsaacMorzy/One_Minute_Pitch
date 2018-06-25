178b95668b0a2dc2a23c36f71993640136b2afdb
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-06-25 10:21:23 +0300

    Add profile pic property to class user and running seocond migration

--- app/models.py
+++ app/models.py
@@ -1,8 +1,16 @@
-from .import db;
+from .import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from .import login_manager
+from sqlalchemy.sql import func 
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    '''
+    @login_manager.user_loader Passes in a user_id to this function
+    Function queries the database and gets a user's id as a response
+    '''
+    return User.query.get(int(user_id))
 class User(UserMixin,db.Model):
     '''
     User class to define a user in the database
@@ -20,6 +28,7 @@ class User(UserMixin,db.Model):
 
     #password column for passwords
     password = db.Column(db.String(100))
+    profile_pic_path = db.Column(db.String())   
 
     @property
     def password(self):
