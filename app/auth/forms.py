6609f9ae8453c3958941a929e0f4bba28b154de2
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-06-25 18:50:28 +0300

    modify the loginform class and registration class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import Required,Email,length,EqualTo
 from ..models import User
+from wtforms import ValidationError
 
 class LoginForm(FlaskForm):
     '''
